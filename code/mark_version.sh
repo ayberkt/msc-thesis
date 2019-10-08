98098b87c4da1c2b652e0e477ac1a87d2d4c5e64
Ayberk Tosun <ayberk.tosun@gmail.com>
2019-10-08 22:12:09 +0200

    Tweak the version-marking script

--- code/mark_version.sh
+++ code/mark_version.sh
@@ -1,3 +1,4 @@
-echo "-- Version: $(git rev-parse --verify --short HEAD)\n" >> tmp
+echo "-- Version: $(git rev-parse --verify --short HEAD)" >  tmp
+echo ""                                                   >> tmp
 cat $1 >> tmp
 mv tmp $1
