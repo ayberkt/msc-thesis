1c7937e725fa08e990bf75e4a3a1a06f59de4d13
Ayberk Tosun <ayberk.tosun@gmail.com>
2020-04-10 18:40:49 +0200

    Check for trailing spaces

--- code/check_cosmetics.py
+++ code/check_cosmetics.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 import sys
 
-res = True
-
-count = 1
+line_num = 1
 
 with open(sys.argv[1], encoding="utf-8") as f:
   for line in f:
     if len(line) > 91:
-      print(line.strip())
-      res = False
-      break
-    count += 1
+      print("Line {} is longer than 90 characters.".format(line_num))
+      sys.exit(1)
 
-if res:
-  print("OK.")
-  sys.exit(0)
-else:
-  print("Line {} is longer than 90 characters.".format(count))
-  sys.exit(1)
+    if not (line == "\n"):
+      if(line[-2].isspace()):
+        print("Line {} has trailing spaces.".format(line_num))
+        sys.exit(1)
+
+    line_num += 1
+
+print("OK.")
+sys.exit(0)
