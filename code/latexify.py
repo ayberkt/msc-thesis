2cf08e64593431dd2d9a97d0801e7a8a33cbad62
Ayberk Tosun <ayberk.tosun@gmail.com>
2020-05-02 15:12:29 +0200

    Add a preliminary setup for including the Agda development in the report

--- code/latexify.py
+++ code/latexify.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import os
+import subprocess
 
 files = [ "Basis.lagda.md",
           "Powerset.agda",
@@ -33,32 +34,35 @@ def latexify_illiterate(fname):
 def latexify(fname):
   print("Processing literate file: " + fname)
 
-  code_block_started = False
   output_file_name   = fname.split(".")[0] + ".lagda"
   output_file        = open("literate-latex/" + output_file_name, "w+")
+
+  output_file.write("\\begin{code}\n")
+
+  code_block_started = False
 
   with open(fname, encoding="utf-8") as f:
     for line in f:
       if line[0:3] == "```":
-        if not code_block_started:
-          output_file.write("\\begin{code}\n")
-          code_block_started = True
-        else:
-          output_file.write("\\end{code}\n")
-          code_block_started = False
+        code_block_started = not code_block_started
       elif line[0:3] == "## ":
+        output_file.write("\\end{code}\n")
         output_file.write("\\subsection{{{}}}\n".format(line[3:].rstrip()))
+        output_file.write("\\begin{code}\n")
       elif line[0:4] == "### ":
+        output_file.write("\\end{code}\n")
         output_file.write("\\subsubsection{{{}}}\n".format(line[4:].rstrip()))
+        output_file.write("\\begin{code}\n")
       else:
-        if code_block_started:
-          output_file.write(line)
+        if line == "\n":
+          output_file.write("\n")
         else:
-          if line == "\n":
-            output_file.write("\n")
+          if not code_block_started:
+            output_file.write("-- " + line)
           else:
             output_file.write(line)
 
+  output_file.write("\\end{code}\n")
   output_file.close()
 
 if __name__ == "__main__":
@@ -68,8 +72,10 @@ if __name__ == "__main__":
   except:
     print("Directory `literate-latex` already exists.")
 
-  for fname in files:
-    if len(fname.split(".")) == 3:
-      latexify(fname)
-    elif len(fname.split(".")) == 2:
-      latexify_illiterate(fname)
+  fname              = sys.argv[1]
+  output_file_name   = fname.split(".")[0] + ".lagda"
+
+  if len(fname.split(".")) == 3:
+    latexify(fname)
+  elif len(fname.split(".")) == 2:
+    latexify_illiterate(fname)
